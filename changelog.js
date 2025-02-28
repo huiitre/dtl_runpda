2560c8c6c84d7d5b8e27f5d50436d7e1bfc7056b
Yanis <yanis.deluca@distrilog.net>
2025-02-28 16:13:22 +0100

    master : changelog

--- changelog.js
+++ changelog.js
@@ -1,4 +1,7 @@
 export const changelog = {
+  '1.9.1': `### [1.9.1] - 28-02-2025
+    - CORRECTIF : Changement de l'en-tête lors de la création du tag sur github
+  `,
   '1.9.0': `### [1.9.0] - 28-02-2025
     - CORRECTIF : Changement de l'url du changelog
     - AJOUT : Création d'un script interne pour convertir le changelog.js en changelog.md
